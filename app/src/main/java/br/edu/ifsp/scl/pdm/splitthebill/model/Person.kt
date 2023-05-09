deaf30058610f982463be04c66edd6db60da9fa1
NathanHermes <hermesnathan8@gmail.com>
2023-05-09 20:04:18 -0300

    :sparkles: feat(model): Create data class Person

---
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/model/Person.kt
@@ -0,0 +1,15 @@
+package br.edu.ifsp.scl.pdm.splitthebill.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity
+data class Person(
+  @PrimaryKey(autoGenerate = true) val id: Int?,
+  var name: String,
+  var totalSpent: Float,
+  var purchasedItems: String,
+): Parcelable
