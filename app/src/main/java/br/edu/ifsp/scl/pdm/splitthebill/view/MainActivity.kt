8fed7417c085fde7df7079c605c55a151f49feea
NathanHermes <hermesnathan8@gmail.com>
2023-05-09 21:30:15 -0300

    :truck: chore: Change package of MainActivity.kt

--- app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.edu.ifsp.scl.pdm.splitthebill
+package br.edu.ifsp.scl.pdm.splitthebill.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.scl.pdm.splitthebill.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
