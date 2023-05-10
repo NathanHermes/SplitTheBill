2164dcbe1ad5c227ea947b569e774dae56b4df8c
NathanHermes <hermesnathan8@gmail.com>
2023-05-09 22:59:59 -0300

    :sparkles: feat(view): Inflate button of menu

--- app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
@@ -2,11 +2,17 @@ package br.edu.ifsp.scl.pdm.splitthebill.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import br.edu.ifsp.scl.pdm.splitthebill.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
