bf226a8c2014a6268aeb0dfaf8e348b5829ed0d9
NathanHermes <hermesnathan8@gmail.com>
2023-05-10 13:05:21 -0300

    :sparkles: feat(adapter): Create logic of person adapter

--- app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/adapter/PersonAdapter.kt
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/adapter/PersonAdapter.kt
@@ -1,4 +1,51 @@
 package br.edu.ifsp.scl.pdm.splitthebill.adapter
 
-class PersonAdapter {
+import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import br.edu.ifsp.scl.pdm.splitthebill.R
+import br.edu.ifsp.scl.pdm.splitthebill.databinding.TilePersonBinding
+import br.edu.ifsp.scl.pdm.splitthebill.model.Person
+
+class PersonAdapter(context: Context, private val people: MutableList<Person>) :
+  ArrayAdapter<Person>(context, R.layout.tile_person, people) {
+  private lateinit var tilePersonBinding: TilePersonBinding
+
+  override fun getView(position: Int, convertView: View?, parent: android.view.ViewGroup): View {
+    val person: Person = people[position]
+    var tilePersonView = convertView
+
+    if (tilePersonView == null) {
+      tilePersonBinding = TilePersonBinding.inflate(
+        context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater,
+        parent, false
+      )
+      tilePersonView = tilePersonBinding.root
+
+      val tilePersonViewHolder = TilePersonViewHolder(
+        tilePersonView.findViewById(R.id.nameTv),
+        tilePersonView.findViewById(R.id.productsTv),
+        tilePersonView.findViewById(R.id.productPrinceTv)
+      )
+
+      tilePersonView.tag = tilePersonViewHolder
+    }
+
+    with(tilePersonView.tag as TilePersonViewHolder) {
+      nameTv.text = person.name
+      productsTv.text = person.purchasedItems
+      productPrinceTv.text = person.totalSpent.toString()
+    }
+
+    return tilePersonView
+  }
+
+  private data class TilePersonViewHolder(
+    val nameTv: TextView,
+    val productsTv: TextView,
+    val productPrinceTv: TextView
+  )
 }
