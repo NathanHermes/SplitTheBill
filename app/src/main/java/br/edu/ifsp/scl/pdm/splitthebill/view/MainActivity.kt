8a82f51d4c23978f6add3e89cae36d6b2aedba44
NathanHermes <hermesnathan8@gmail.com>
2023-05-11 09:20:33 -0300

    :sparkles: feat(view): Create logic to insert person in database

--- app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.ifsp.scl.pdm.splitthebill.R
 import br.edu.ifsp.scl.pdm.splitthebill.adapter.PersonAdapter
+import br.edu.ifsp.scl.pdm.splitthebill.controller.PersonController
 import br.edu.ifsp.scl.pdm.splitthebill.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.pdm.splitthebill.model.Person
 
@@ -21,23 +23,39 @@ class MainActivity : BaseActivity() {
   private val personAdapter: PersonAdapter by lazy {
     PersonAdapter(this, people)
   }
+  private val personController: PersonController by lazy {
+    PersonController(this)
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(activityMainBinding.root)
 
-    fillContactList()
     with(activityMainBinding) {
+      personController.findAllPeople()
       peopleLv.adapter = personAdapter
 
       personActivityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-
           if (result.resultCode == RESULT_OK) {
             val person = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
               result.data?.getParcelableExtra(EXTRA_NEW_PERSON, Person::class.java)
             } else {
               result.data?.getParcelableExtra(EXTRA_NEW_PERSON)
+            }
+
+            person?.let { _person ->
+              val positon = people.indexOfFirst { it.id == _person.id }
+
+              if (positon != -1) {
+
+              } else {
+                personController.createPerson(_person)
+                Toast.makeText(this@MainActivity, "Integrante adicionado.", Toast.LENGTH_LONG)
+                  .show()
+              }
+
+              personAdapter.notifyDataSetChanged()
             }
           }
         }
@@ -63,7 +81,13 @@ class MainActivity : BaseActivity() {
     }
   }
 
-  private fun fillContactList() {
+  fun updatePeople(_people: MutableList<Person>) {
+    people.clear()
+    people.addAll(_people)
+    personAdapter.notifyDataSetChanged()
+  }
+
+  /*private fun fillContactList() {
     for (index: Int in 1..20) {
       people.add(
         Person(
@@ -71,5 +95,5 @@ class MainActivity : BaseActivity() {
         )
       )
     }
-  }
+  }*/
 }
