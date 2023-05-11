ce033d62e48eefd7a42ea79474e4bac983fb6f4d
NathanHermes <hermesnathan8@gmail.com>
2023-05-11 00:57:02 -0300

    :sparkles: feat(view): Create logic to open PersonActivity

--- app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/MainActivity.kt
@@ -1,8 +1,12 @@
 package br.edu.ifsp.scl.pdm.splitthebill.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.ifsp.scl.pdm.splitthebill.R
 import br.edu.ifsp.scl.pdm.splitthebill.adapter.PersonAdapter
 import br.edu.ifsp.scl.pdm.splitthebill.databinding.ActivityMainBinding
@@ -12,6 +16,7 @@ class MainActivity : AppCompatActivity() {
   private val activityMainBinding: ActivityMainBinding by lazy {
     ActivityMainBinding.inflate(layoutInflater)
   }
+  private lateinit var personActivityResultLauncher: ActivityResultLauncher<Intent>
   private val people: MutableList<Person> = mutableListOf()
   private val personAdapter: PersonAdapter by lazy {
     PersonAdapter(this, people)
@@ -21,16 +26,33 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(activityMainBinding.root)
 
-    fillContactList();
+    fillContactList()
     with(activityMainBinding) {
       peopleLv.adapter = personAdapter
-    }
 
+      personActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        println(result)
+      }
+
+      registerForContextMenu(peopleLv)
+    }
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
     menuInflater.inflate(R.menu.menu_main, menu)
     return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      R.id.addPersonMi -> {
+        val personIntent = Intent(this, PersonActivity::class.java)
+        personActivityResultLauncher.launch(personIntent)
+        true
+      }
+
+      else -> false
+    }
   }
 
   private fun fillContactList() {
