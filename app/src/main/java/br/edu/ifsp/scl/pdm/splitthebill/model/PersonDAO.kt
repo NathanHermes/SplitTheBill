8e831b3bc50165f622945ed8d9cee559d2b56ca1
NathanHermes <hermesnathan8@gmail.com>
2023-05-13 01:46:35 -0300

    :art: style(model): Add blank lines

--- app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/model/PersonDAO.kt
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/model/PersonDAO.kt
@@ -6,12 +6,16 @@ import androidx.room.*
 interface PersonDAO {
   @Query("SELECT * FROM Person WHERE id = :id")
   fun findPersonById(id: Int): Person?
+
   @Query("SELECT * FROM Person")
   fun findAll(): MutableList<Person>
+
   @Insert
   fun create(person: Person)
+
   @Update
   fun update(person: Person)
+
   @Delete
   fun delete(person: Person)
 }
