c7bd1d9dd7ae37e6f9f49923415ccdc74e9a6d0e
NathanHermes <hermesnathan8@gmail.com>
2023-05-13 01:42:15 -0300

    :sparkles: feat(view): Create and rename global constants

--- app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/BaseActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/pdm/splitthebill/view/BaseActivity.kt
@@ -3,6 +3,7 @@ package br.edu.ifsp.scl.pdm.splitthebill.view
 import androidx.appcompat.app.AppCompatActivity
 
 sealed class BaseActivity: AppCompatActivity() {
-  protected val EXTRA_NEW_PERSON = "NewPerson"
+  protected val EXTRA_PERSON = "Person"
+  protected val EXTRA_VIEW_PERSON = "ViewPerson"
   protected val EXTRA_PEOPLE = "People"
 }
